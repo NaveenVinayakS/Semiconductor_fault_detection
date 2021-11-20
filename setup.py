a38e508144caa888119754b1971221633c407856
snv1995 <naveenvinayak.2195@gmail.com>
2021-11-20 17:16:18 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="docker-heroku",
+    version="0.0.3",
+    description="ML project",
+    author="Naveen Vinayak",
+    packages=find_packages(),
+    license="MIT"
+)
